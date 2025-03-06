3a1ee56f526645fafc7f891c6f9f4de0c3e05614
Shubham Chaubey <shubham.chaubey124985@marwadiuniversity.ac.in>
2025-03-06 19:09:21 +0530

    C++ basic program

--- prac-4.cpp
+++ prac-4.cpp
@@ -2,3 +2,17 @@
 
 #include <iostream>
 using namespace std;
+
+int main(){
+    int a, b, temp;
+
+    cout << "Enter two number : ";
+    cin >> a >> b;
+
+    cout << " Before swapping : a = " << a << " b = " << b << endl;
+    temp = a;
+    a = b;
+    b = temp;
+    cout << " After swapping : a = " << a << " b = " << b <<endl;
+    return 0;
+}
