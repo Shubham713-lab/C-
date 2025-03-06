92be6142ae6b3e8e15c035314d69ad236e64af8a
Shubham Chaubey <shubham.chaubey124985@marwadiuniversity.ac.in>
2025-03-06 22:12:50 +0530

    prime number

---
+++ prac-7.cpp
@@ -0,0 +1,32 @@
+// Write a C++ Program to check whether prime number or not.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+
+    cout << "Enter a number : ";
+    cin >> a;
+
+    bool isPrime = true;
+
+    for (int i = 2; i <= a / 2; i++)
+    {
+        if (a % i == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime)
+    {
+        cout << "it is prime number";
+    }
+    else
+    {
+        cout << "it is not prime";
+    }
+    return 0;
+}
