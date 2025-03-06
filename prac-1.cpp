4df92ec43de48e850d973ebb7ec23167d05d7515
Shubham Chaubey <shubham.chaubey124985@marwadiuniversity.ac.in>
2025-03-06 23:51:32 +0530

    odd or even

--- prac-1.cpp
+++ prac-1.cpp
@@ -3,18 +3,20 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int a;
 
     cout << "Enter a number : ";
     cin >> a;
 
-    if (a%2 == 0){
+    if (a % 2 == 0)
+    {
         cout << a << " is even number.";
     }
-        
-    
-    else{
+
+    else
+    {
         cout << a << " is odd number.";
     }
 
