c476902fe057f09ea3bbba35d1d6129a8d1727ee
Shubham Chaubey <shubham.chaubey124985@marwadiuniversity.ac.in>
2025-03-08 09:32:44 +0530

    Refactor complex number addition program to use a class and friend function

--- prac-26.cpp
+++ prac-26.cpp
@@ -3,28 +3,21 @@
 #include <iostream>
 using namespace std;
 
-class sum
-{
-    int img1, img2, num1, num2;
-
+class Complex {
+    float real, imag;
 public:
-    int setValues()
-    {
-        cout << "Enter first complex no. : ";
-        cin >> num1 >> img1;
-        cout << "Enter second complex no. : ";
-        cin >> num2 >> img2;
-    }
-    int add()
-    {
-        cout << "The sum of complex no. is : " << num1 + num2 << " + " << img1 + img2 << "i";
-    }
+    Complex(float r = 0, float i = 0) : real(r), imag(i) {}
+    friend Complex addComplex(Complex, Complex);
+    void display() { cout << "The sum is : " << real << " + " << imag << "i" << endl; }
 };
 
-int main()
-{
-    sum obj;
-    obj.setValues();
-    obj.add();
+Complex addComplex(Complex c1, Complex c2) {
+    return Complex(c1.real + c2.real, c1.imag + c2.imag);
+}
+
+int main() {
+    Complex c1(3.2, 2.5), c2(1.5, 4.5);
+    Complex sum = addComplex(c1, c2);
+    sum.display();
     return 0;
 }
